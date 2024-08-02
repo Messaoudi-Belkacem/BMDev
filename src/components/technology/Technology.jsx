4f1f03a415502cf4ad2bd23527545d3308fe2efa
HP <wailmessaoudi806@gmail.com>
2024-08-02 19:32:37 +0100

    Finnish up skills section

--- src/components/technology/Technology.jsx
+++ src/components/technology/Technology.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './technology.css';
 
-const Technology = ({ imageSrc, name, href }) => {
+const Technology = ({ imageSrc, name, href}) => {
   return (
-    <a href={href} target='_blank'>
-      <div className="__bmdev-technology">
-        <img src={imageSrc} alt={name} className="technology-image" />
-        <p className="technology-name">{name}</p>
-      </div>
+    <a href={href} target='_blank' className='__bmdev-technology'>
+      <img src={imageSrc} alt={name} className='technology-image' />
+      <p className='technology-name'>{name}</p>
     </a>
   );
 };
@@ -17,7 +15,7 @@ const Technology = ({ imageSrc, name, href }) => {
 Technology.propTypes = {
   imageSrc: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  href: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired
 };
 
 export default Technology;
--- src/conatiners/skills/Skills.jsx
+++ src/conatiners/skills/Skills.jsx
@@ -2,14 +2,32 @@ import {Technology} from '../../components';
 import javascriptIcon from '../../assets/icon-javascript.svg'
 import reactIcon from '../../assets/icon-react.svg'
 import nodejsIcon from '../../assets/icon-nodejs.svg'
+import javaIcon from '../../assets/icon-java.svg'
+import springIcon from '../../assets/icon-spring.svg'
+import cIcon from '../../assets/icon-c.svg'
+import kotlinIcon from '../../assets/icon-kotlin.svg'
+import composeIcon from '../../assets/icon-compose.svg'
+import htmlIcon from '../../assets/icon-html.svg'
+import cssIcon from '../../assets/icon-css.svg'
+import pythonIcon from '../../assets/icon-python.svg'
+import flaskIcon from '../../assets/icon-flask.svg'
 import './skills.css'
 
 
 function Skills() {
     const technologies = [
-        { name: 'Javascript', imageSrc: javascriptIcon, href: '' },
-        { name: 'React', imageSrc: reactIcon, href: '' },
-        { name: 'Node.js', imageSrc: nodejsIcon, href: '' },
+        { name: 'Javascript', imageSrc: javascriptIcon, href: 'https://www.javascript.com/' },
+        { name: 'React', imageSrc: reactIcon, href: 'https://react.dev/' },
+        { name: 'Node.js', imageSrc: nodejsIcon, href: 'https://nodejs.org/en' },
+        { name: 'Java', imageSrc: javaIcon, href: 'https://www.java.com/en/' },
+        { name: 'Spring', imageSrc: springIcon, href: 'https://spring.io/' },
+        { name: 'C', imageSrc: cIcon, href: 'https://www.learn-c.org/' },
+        { name: 'Kotlin', imageSrc: kotlinIcon, href: 'https://kotlinlang.org/' },
+        { name: 'Jetpack Compose', imageSrc: composeIcon, href: 'https://developer.android.com/compose' },
+        { name: 'HTML', imageSrc: htmlIcon, href: 'https://www.w3schools.com/html/' },
+        { name: 'CSS', imageSrc: cssIcon, href: 'https://web.dev/learn/css' },
+        { name: 'Python', imageSrc: pythonIcon, href: 'https://www.python.org/' },
+        { name: 'Flask', imageSrc: flaskIcon, href: 'https://flask.palletsprojects.com/en/3.0.x/' },
       ];
     return(
         <div className='bmdev__skills'>
