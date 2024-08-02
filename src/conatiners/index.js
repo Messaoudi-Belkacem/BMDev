427111cc3286626a0d9bef38939af04503af1388
HP <wailmessaoudi806@gmail.com>
2024-08-02 19:42:34 +0100

    Add work conatiner

--- src/conatiners/index.js
+++ src/conatiners/index.js
@@ -1,2 +1,3 @@
 export { default as Hero } from './hero/Hero';
 export { default as Skills } from './skills/Skills';
+export { default as Work } from './work/Work';
---
+++ src/conatiners/work/Work.jsx
@@ -0,0 +1,11 @@
+import './work.css'
+
+function Work() {
+    return(
+        <div className='bmdev__work'>
+            
+        </div>
+    )
+}
+
+export default Work
